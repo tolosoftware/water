d78f372ae9e585331064c006070f05a4c43273d9
naseri <mahdi.naseri1376@gmail.com>
2021-02-21 11:58:04 +0430

    mahdi naseri

--- water-solar/src/app/routes/newproject/index.js
+++ water-solar/src/app/routes/newproject/index.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import ContainerHeader from "components/ContainerHeader/index";
 import IntlMessages from "util/IntlMessages";
+import VerticalTabs from './VerticalTabs';
 
 const Newproject = ({match}) => (
     <div className="app-wrapper">
-   <ContainerHeader match={match} title={<IntlMessages id="New Project"/>}/>      
-      <h1>This is New Project</h1>
+    <ContainerHeader match={match} title={<IntlMessages id="New Project" />} />   
+ 
+     <VerticalTabs/>
   </div>
 );
 
