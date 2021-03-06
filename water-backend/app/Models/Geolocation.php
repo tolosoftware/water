d673081ed4b953d8149c1b5346daf30576212233
unknown <valy.nabizada@gmail.com>
2021-03-06 17:10:25 +0430

    saturday work

--- water-backend/app/Models/Geolocation.php
+++ water-backend/app/Models/Geolocation.php
@@ -5,19 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class Geolocation extends Model
+class GeoLocation extends Model
 {
     use HasFactory;
-    protected $table = "geolocations";
-    protected $primaryKey = "id";
-    protected $fillable = [
-        'country', 'city','latitude','longtitude'
-     ];
-
-      public function projects()
-    {
-        return $this->hasMany(Projects::class, 'city_id');
-    }
-
- 
 }
