22ca62c0d759e54604708b59500b8a7b50a61043
unknown <valy.nabizada@gmail.com>
2021-05-22 09:12:08 +0430

    final upadate on project summary and preview

--- water-solar/src/app/routes/newproject/Preview.js
+++ water-solar/src/app/routes/newproject/Preview.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import Button from '@material-ui/core/Button';
 import Dialog from '@material-ui/core/Dialog';
@@ -12,6 +12,7 @@ import IconButton from '@material-ui/core/IconButton';
 import Typography from '@material-ui/core/Typography';
 import CloseIcon from '@material-ui/icons/Close';
 import Slide from '@material-ui/core/Slide';
+// import Summaryheader
 
 const useStyles = makeStyles((theme) => ({
   appBar: {
@@ -29,7 +30,13 @@ const Transition = React.forwardRef(function Transition(props, ref) {
 
 export default function Preview(props) {
   const classes = useStyles();
-  const {open, setOpen} = props;
+  const {open, setOpen, previewData} = props;
+
+  useEffect(() => {
+    if(previewData){
+      console.log('preview Data in preview page', previewData);
+    }
+  }, [previewData]);
 
   const handleClickOpen = () => {
     setOpen(true);
